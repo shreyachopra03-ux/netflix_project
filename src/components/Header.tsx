60dd57978531f34af444afb0a49d9226d7fb4dca
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-04 22:20:09 +0530

    add sign out logic

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,56 +1,22 @@
-// const Header = () => {
-//     return (
-//         <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10">
-//           <img src ="/netflix-logo.png"
-//           alt="logo"
-//            />
-//         <div className="flex p-2">
-//             <img src="/user-icon.png"
-//             className="usericon w-12 h-12"
-//             alt="usericon"
-//             />
-//             <button>Sign Out</button>
-//         </div>
-//         </div>
-//     )
-// }
-
-// export default Header;
-
-
-// const Header = () => {
-//   return (
-//     <div className="absolute top-0 left-0 w-full px-8 py-2 flex justify-between items-center bg-gradient-to-b from-black z-20">
-      
-//       <img
-//         src="/netflix-logo.png"
-//         alt="logo"
-//         className="w-44"
-//       />
-
-//       <div className="flex items-center gap-4">
-//         <img
-//           src="/user-icon.png"
-//           className="w-10 h-10 rounded-md"
-//           alt="usericon"
-//         />
-
-//         <button className="bg-red-600 text-white px-4 py-1 rounded-md hover:bg-red-700">
-//           Sign Out
-//         </button>
-//       </div>
-
-//     </div>
-//   );
-// };
-
-// export default Header;
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
 
 interface HeaderProps {
   showUser?: boolean;
 }
 
 const Header = ({ showUser = false }: HeaderProps) => {
+    const navigate = useNavigate()
+    const handleSignOut = () => {
+        signOut(auth).then(() => {
+           navigate("/");
+        })
+        .catch((error) => {
+            navigate("/error");
+        });
+
+    }
   return (
     <div className="absolute top-0 left-0 w-full px-8 py-4 flex justify-between items-center bg-gradient-to-b from-black z-20">
 
@@ -68,7 +34,9 @@ const Header = ({ showUser = false }: HeaderProps) => {
             alt="usericon"
           />
 
-          <button className="bg-red-600 px-4 py-1 rounded text-white hover:bg-red-700">
+          <button 
+          onClick={handleSignOut}
+          className="bg-red-600 px-4 py-1 rounded text-white hover:bg-red-700">
             Sign Out
           </button>
         </div>
