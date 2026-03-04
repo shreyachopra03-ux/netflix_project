4253270ad4a2a0a31ea5a252f86855b16d893845
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-04 22:04:28 +0530

    fix: browse component

--- src/components/Browse.tsx
+++ src/components/Browse.tsx
@@ -1,11 +1,23 @@
+// import Header from "./Header";
+
+// const Browse = () => {
+//     return (
+//         <div>
+//             <Header/>
+//         </div>
+//     )
+// }
+
+// export default Browse;
+
 import Header from "./Header";
 
 const Browse = () => {
-    return (
-        <div>
-            <Header/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Header showUser={true} />
+    </div>
+  );
+};
 
 export default Browse;
