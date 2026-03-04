00f0d9c3845380adfb850c1d2ae5b6f535c7b500
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-05 02:09:36 +0530

    App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,3 @@
-// import Body from "./components/Body";
-// import  appStore  from "./utils/appStore";
-// import { Provider } from "react-redux";
-
-// function App() {
-//   return (
-//     <Provider store={appStore}>
-//     <Body />
-//     </Provider>
-//   );
-// }
-
-// export default App;
-
 import { useEffect } from "react";
 import Body from "./components/Body";
 import appStore from "./utils/appStore";
