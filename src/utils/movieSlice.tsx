071d10e15cabda887423c9c007ac0252b658f2cc
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-06 01:44:18 +0530

    movies slice

---
+++ src/utils/movieSlice.tsx
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+
+const movieSlice = createSlice({
+    name:"movies",
+    initialState: {
+        nowPlayingMovies: null as string | null,
+    },
+    reducers: {
+        addNowPlayingMovies: (state, action:PayloadAction<string | null>) => {
+            state.nowPlayingMovies = action.payload;
+        }
+    }
+})
+
+
+export const { addNowPlayingMovies } = movieSlice.actions;
+export default movieSlice.reducer;
