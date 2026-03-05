ff8a963579ba02689d58981f13b6ebd2214216fc
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-05 16:32:31 +0530

    constants file

---
+++ src/utils/constants.tsx
@@ -0,0 +1,7 @@
+export const LOGO = "/netflix-logo.png";
+
+export const USER_AVATAR = "/user-icon.png";
+
+export const BACKGROUND_IMG = "https://assets.nflxext.com/ffe/siteui/vlv3/e49aba81-ee7c-4f19-baef-7c54bbab003e/web/IN-en-20260202-TRIFECTA-perspective_04f5de39-b518-493c-9a8d-6aef11af0457_large.jpg"
+
+// export const 
