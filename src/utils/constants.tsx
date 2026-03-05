9bc582f34a31fdf9f4ac315ed511d12a78d6e96b
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-06 01:16:50 +0530

    update the constants file

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -4,4 +4,10 @@ export const USER_AVATAR = "/user-icon.png";
 
 export const BACKGROUND_IMG = "https://assets.nflxext.com/ffe/siteui/vlv3/e49aba81-ee7c-4f19-baef-7c54bbab003e/web/IN-en-20260202-TRIFECTA-perspective_04f5de39-b518-493c-9a8d-6aef11af0457_large.jpg"
 
-// export const 
+export const API_OPTIONS = {
+       method: 'GET',
+       headers: {
+        accept: "application/json",
+        Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OGQ1YTQ2ZDQ1YTBmNzNjZGI5YTE1MjBiMzU3NDVkMiIsIm5iZiI6MTc3MjcwODc2OC45ODIsInN1YiI6IjY5YTk2M2EwY2MxNmUxYmRjZThmOWZiOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.s92nN_8fg-afmUPx8yZNyILlwpmkNMnq0w1nugvsWDs"
+       },
+    };
