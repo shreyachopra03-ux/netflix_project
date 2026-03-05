17034258de7b0b5968d66f84d7bad1f711956b55
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-06 01:16:01 +0530

    consoled log the json results

--- src/components/Browse.tsx
+++ src/components/Browse.tsx
@@ -1,6 +1,21 @@
 import Header from "./Header";
+import { API_OPTIONS } from "../utils/constants";
+import { useEffect } from "react";
 
 const Browse = () => {
+
+  const getNowPlayingMovies = async () => {
+    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?Page=1',
+    API_OPTIONS
+    );
+    const json = await data.json();
+    console.log(json.results);
+  };
+
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, [])
+
   return (
     <div>
       <Header showUser={true} />
