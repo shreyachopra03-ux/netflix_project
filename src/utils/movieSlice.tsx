8952fc521713ebdecf9cc5d3887282c02d1a88d9
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-06 17:12:10 +0530

    Movie Slice

--- src/utils/movieSlice.tsx
+++ src/utils/movieSlice.tsx
@@ -1,22 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
+type Movie = {
+    id: number
+    original_title: string
+    overview: string
+}
+
+type Trailer = {
+    key: string
+}
+
 const movieSlice = createSlice({
     name:"movies",
     initialState: {
-        nowPlayingMovies: null as string | null,
-        trailerVideo: null as string | null
+        nowPlayingMovies:  [] as Movie[],
+        trailerVideo: null as Trailer | null
     },
     reducers: {
-        addNowPlayingMovies: (state, action:PayloadAction<string | null>) => {
+        addNowPlayingMovies: (state, action:PayloadAction<Movie[]>) => {
             state.nowPlayingMovies = action.payload;
         },
-        addTrailerVideo: (state, action:PayloadAction<string | null>) => {
+        addTrailerVideo: (state, action:PayloadAction<Trailer>) => {
             state.trailerVideo = action.payload;
         }
     }
 })
-
 
 export const { addNowPlayingMovies, addTrailerVideo } = movieSlice.actions;
 export default movieSlice.reducer;
