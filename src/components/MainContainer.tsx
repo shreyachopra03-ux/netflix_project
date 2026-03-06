6d1ac572690944c69f54cf64c6095b6e12fc2382
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-06 13:04:08 +0530

    destructuring the props of movieList

---
+++ src/components/MainContainer.tsx
@@ -0,0 +1,25 @@
+import { useSelector } from "react-redux";
+import type { RootState } from "../utils/appStore";
+import VideoBackground from "./VideoBackground";
+import VideoTitle from "./VideoTitle";
+
+const MainContainer = () => {
+    const movies = useSelector((store:RootState) => store.movies?.nowPlayingMovies);
+
+    if (!movies) return;
+
+    const mainMovie = movies[0];
+    console.log(mainMovie);
+
+    const { original_title, overview, id} = mainMovie;
+
+ return (
+    <div>
+        <VideoTitle title={original_title} overview={overview} />
+        <VideoBackground movieId={id} />
+      
+    </div>
+ )
+}
+
+export default MainContainer;
