0668fde52d73266084a812ffd6b1c94f625d567f
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-06 15:45:44 +0530

    Movie Slice

--- src/utils/movieSlice.tsx
+++ src/utils/movieSlice.tsx
@@ -4,15 +4,19 @@ import type { PayloadAction } from "@reduxjs/toolkit";
 const movieSlice = createSlice({
     name:"movies",
     initialState: {
-        nowPlayingMovies: null as string | null
+        nowPlayingMovies: null as string | null,
+        trailerVideo: null as string | null
     },
     reducers: {
         addNowPlayingMovies: (state, action:PayloadAction<string | null>) => {
             state.nowPlayingMovies = action.payload;
+        },
+        addTrailerVideo: (state, action:PayloadAction<string | null>) => {
+            state.trailerVideo = action.payload;
         }
     }
 })
 
 
-export const { addNowPlayingMovies } = movieSlice.actions;
+export const { addNowPlayingMovies, addTrailerVideo } = movieSlice.actions;
 export default movieSlice.reducer;
