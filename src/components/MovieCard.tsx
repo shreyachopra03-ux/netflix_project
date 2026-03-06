9dad697d74ed7f06e122c6a14335ebf8ae2cb541
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-06 20:03:33 +0530

    Movie card component

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,7 +1,17 @@
-const MovieCard = () => {
-    return (
-        <div>
+import { IMG_CDN_URL } from "../utils/constants";
 
+type Props = {
+    posterPath: string
+}
+
+const MovieCard = ({ posterPath }:Props) => {
+    console.log(posterPath)
+    return (
+        <div className="w-36">
+            <img
+            className="w-full"
+            alt="Movie Card"
+            src={IMG_CDN_URL  + posterPath} />
         </div>
     )
 }
