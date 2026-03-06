45d53b800e06f425f7a99c0f9459c7677f9fcd37
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-06 20:03:07 +0530

    Movie List component

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,12 +1,22 @@
 import MovieCard from "../components/MovieCard";
 
-const MovieList = ({ title, movies }) => {
+export type Movie = {
+  id: number;
+  poster_path: string;
+}
+
+type Props = {
+    title: string;
+    movies:  Movie[]
+}
+
+const MovieList = ({ title, movies }:Props) => {
     return (
         <div>
             <div>
                 <h1>{title}</h1>
                 <div>
-                   <MovieCard/>
+                    <MovieCard posterPath={movies?.[0]?.poster_path}/>
                 </div>
             </div>
         </div>
