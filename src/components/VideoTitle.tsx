2b884b94839ef1df9dee1632acdb8bab7b2580b7
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-06 15:44:41 +0530

    Video Title Of Main Container

--- src/components/VideoTitle.tsx
+++ src/components/VideoTitle.tsx
@@ -1,14 +1,14 @@
 const VideoTitle = ({title, overview}) => {
-    
+
     return (
-        <div className="pt-36 px-12">
+        <div className="w-screen aspect-video bg-gradient-to-r from-black pt-[20%] px-24 absolute text-white">
             <h1 className="text-4xl font-bold">{title}</h1>
             <p className="py-6 text-m w-1/4">{overview}</p>
             <div className="">
-            <button className="bg-gray-400 text-white p-4 px-10 text-m bg-opacity-50 rounded-lg">
+            <button className="bg-white text-black p-4 px-12 text-lg hover:bg-white/80 rounded-lg">
                 ▶️ Play
             </button>
-            <button className="bg-gray-400 mx-2 text-white p-4 px-10 text-m bg-opacity-50 rounded-lg">
+            <button className="bg-gray-400 mx-2 text-white p-4 px-10 text-lg hover:bg-gray-400/80 rounded-lg">
                 More Info
             </button>
             </div>
