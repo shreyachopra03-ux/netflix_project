358b52026877af621ef4156e58c718f362134efc
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-06 20:26:22 +0530

    constants

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -13,6 +13,7 @@ export const API_OPTIONS = {
     };
 
 
-export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w700";
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
 
 // export const POSTER_PATH = "/buPFnHZ3xQy6vZEHxbHgL1Pc6CR.jpg";
+
