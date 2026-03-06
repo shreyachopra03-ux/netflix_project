e3151c2b166bf63256668e85d8d285f2d6ef9f0e
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-06 15:44:10 +0530

    Video Bg Of Main Container

--- src/components/VideoBackground.tsx
+++ src/components/VideoBackground.tsx
@@ -1,35 +1,21 @@
-import { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constants";
+import type { RootState } from "../utils/appStore";
+import { useSelector } from "react-redux";
+import  useMovieTrailer from "../hooks/useMovieTrailer"
 
 const VideoBackground = ({ movieId }) => {
+    const trailerVideo = useSelector((store):RootState => store.movies?.trailerVideo)
+    // console.log(trailerVideo);
 
-    // fetch trailer video
-    const getMovieVideos = async () => {
-        const data = await fetch ("https://api.themoviedb.org/3/movie/1290821/videos",
-            API_OPTIONS
-        );
-        const json = await data.json();
-        console.log(json);
-
-        const filterData = json.results.filter((video) => video.type == "Trailer");
-
-        // If there's a trailer video then take that, otherwise return the 1st video out of all the resulting videos
-        const trailer = filterData.length ? filterData[0] : json.results[0]
-        console.log(trailer);
-    }
-
-    useEffect(() => {
-        getMovieVideos();
-    }, [])
+    useMovieTrailer(movieId); 
 
     return (
-        <div>
-        <iframe width="560" height="315"
-        src="https://www.youtube.com/embed/auHiLFjaIc0?si=7wNZGwGG79VmlIRw" 
+        <div className="">
+        <iframe
+        className="w-screen aspect-video" 
+        src= { "https://www.youtube.com/embed/" + trailerVideo?.key + "?&autoplay=1&mute=1"}
         title="YouTube video player" 
-        frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+        referrerPolicy="strict-origin-when-cross-origin">
         </iframe>  
         </div>
     )
