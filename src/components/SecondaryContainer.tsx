bddd784bd6a8a5aa78ff76f39155aa6a5ac34d5d
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-06 17:18:55 +0530

    Secondary Container for movie lists and movie cards

--- src/components/SecondaryContainer.tsx
+++ src/components/SecondaryContainer.tsx
@@ -1,12 +1,13 @@
 import { useSelector } from "react-redux";
 import MovieList from "./MovieList";
+import type { RootState } from "../utils/appStore";
 
 const SecondaryContainer = () => {
-    const movies = useSelector((store) => store.movies);
+    const movies = useSelector((store: RootState) => store.movies);
 
     return (
         <div>
-            <MovieList title={"Now Playing"} movies={movies.nowPlaying}/>
+            <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies}/>
         </div>
     )
 }
