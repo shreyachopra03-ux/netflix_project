5c0bdc4291907e711960d2d41bf5864694b6f20d
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-06 22:46:23 +0530

    Constants file

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -8,12 +8,10 @@ export const API_OPTIONS = {
        method: 'GET',
        headers: {
         accept: "application/json",
-        Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OGQ1YTQ2ZDQ1YTBmNzNjZGI5YTE1MjBiMzU3NDVkMiIsIm5iZiI6MTc3MjcwODc2OC45ODIsInN1YiI6IjY5YTk2M2EwY2MxNmUxYmRjZThmOWZiOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.s92nN_8fg-afmUPx8yZNyILlwpmkNMnq0w1nugvsWDs"
+        Authorization: `Bearer ${import.meta.env.VITE_TMDB_ACCESS_TOKEN}`
        },
     };
 
-
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
 
-// export const POSTER_PATH = "/buPFnHZ3xQy6vZEHxbHgL1Pc6CR.jpg";
 
