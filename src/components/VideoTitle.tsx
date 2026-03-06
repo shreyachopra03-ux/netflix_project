e7bb528c431278efbc9624c94041ba8569300075
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-06 13:06:09 +0530

    Video title

---
+++ src/components/VideoTitle.tsx
@@ -0,0 +1,18 @@
+const VideoTitle = ({title, overview}) => {
+    return (
+        <div className="pt-36 px-12">
+            <h1 className="text-4xl font-bold">{title}</h1>
+            <p className="py-6 text-m w-1/4">{overview}</p>
+            <div className="">
+            <button className="bg-gray-400 text-white p-4 px-10 text-m bg-opacity-50 rounded-lg">
+                ▶️ Play
+            </button>
+            <button className="bg-gray-400 mx-2 text-white p-4 px-10 text-m bg-opacity-50 rounded-lg">
+                More Info
+            </button>
+            </div>
+        </div>
+    )
+}
+
+export default VideoTitle;
