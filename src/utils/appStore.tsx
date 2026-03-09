4a36b188f4dd74956b4047ae12ac7b8b8428beaf
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-09 13:01:25 +0530

    update: add gptReducer to the app store

--- src/utils/appStore.tsx
+++ src/utils/appStore.tsx
@@ -1,15 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import moviesReducer from "./movieSlice";
+import gptReducer from "./gptSlice";
 
- const appStore = configureStore(
-    {
+ const appStore = configureStore({
      reducer: {
         user: userReducer,
-        movies: moviesReducer
-     }
-    }
+        movies: moviesReducer,
+        gpt: gptReducer
+     }}
 )
-export type RootState = ReturnType<typeof appStore.getState>;
 
+export type RootState = ReturnType<typeof appStore.getState>;
 export default appStore;
