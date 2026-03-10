4ba990688dc0c1691ef8e8e42cbee65e6b69c731
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-10 19:22:22 +0530

    feat: add style to the background image

--- src/components/GptSearchPage.tsx
+++ src/components/GptSearchPage.tsx
@@ -1,9 +1,20 @@
-const GptSearch = () => {
+import GptSearchBar  from "../components/GptSearchBar.tsx";
+import GptMovieSuggestion  from "../components/GptMovieSuggestion.tsx";
+import { BACKGROUND_IMG } from "../utils/constants.tsx";
+  
+const GptSearchPage = () => {
     return (
-        <div>Gpt Search Bar
-            GptMovieSuggestion
+        <div>
+            <div className="absolute -z-10">
+            <img
+            src={BACKGROUND_IMG}
+            alt="background"
+            />
+            </div>
+        <GptSearchBar />
+        <GptMovieSuggestion />
         </div>
     )
 }
 
-export default GptSearch;
+export default GptSearchPage;
