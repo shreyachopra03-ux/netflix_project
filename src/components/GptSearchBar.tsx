6969aacb81109e9b72fa2f81242b68a6de0df799
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-10 23:06:33 +0530

    feat: add selector for language state

--- src/components/GptSearchBar.tsx
+++ src/components/GptSearchBar.tsx
@@ -1,16 +1,21 @@
 import lang from "../utils/languageConstants";
+import { useSelector } from "react-redux";
+import type { RootState } from "../utils/appStore";
 
 const GptSearchBar = () => {
+
+    const langKey = useSelector((store: RootState) => store.config.lang) as keyof typeof lang;
+
     return (
         <div className="pt-[10%] flex justify-center">
             <form className="w-1/2 bg-black grid grid-cols-12">
             <input 
             type="text"
             className="p-4 m-4 col-span-9 text-white border-white border-2"
-            placeholder={lang.hindi.gptSearchPlaceholder}
+            placeholder={lang[langKey].gptSearchPlaceholder}
             />
             <button className="col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded-lg border-white border-2">
-               {lang.hindi.search}
+               {lang[langKey].search}
             </button>
             </form>
         </div>
