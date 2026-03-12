a238a7299123ea2b3a097f3091eb069cf2c38e78
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-12 21:13:38 +0530

    feat: style the search results of movie suggestion

--- src/components/GptMovieSuggestion.tsx
+++ src/components/GptMovieSuggestion.tsx
@@ -1,7 +1,22 @@
-const GptMovieSuggestion = () => {
-    return (
-        <div>
+import { useSelector } from "react-redux";
+import MovieList from "./MovieList";
+import type { RootState } from "../utils/appStore";
 
+const GptMovieSuggestion = () => {
+    const { movieResults, movieNames } = useSelector((store: RootState) => store.gpt);
+    if (!movieNames || !movieResults) return null;
+
+    return (
+        <div className="p-4 m-4 text-white bg-black/80">
+        <div>
+        {movieNames.map((movieName, index) => (
+            <MovieList
+               key={movieName}
+               title={movieName}
+               movies={movieResults[index]}
+            />
+        ))}
+        </div>
         </div>
     )
 }
