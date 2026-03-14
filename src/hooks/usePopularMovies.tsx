776b4373384011b44ee97edbe34db1b16c4a2ae5
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-15 02:31:47 +0530

    feat: modify the hook

--- src/hooks/usePopularMovies.tsx
+++ src/hooks/usePopularMovies.tsx
@@ -12,7 +12,7 @@ const usePopularMovies = () => {
    const  popularMovies = useSelector((store: RootState) => store.movies.popularMovies);
 
   const getPopularMovies = async () => {
-    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?Page=1',
+    const data = await fetch('https://api.themoviedb.org/3/movie/popular?page=1',
     API_OPTIONS
     );
     const json = await data.json();
