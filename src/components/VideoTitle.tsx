eccceb6d60a9cc0b124f2bf43d8c1f59cf252942
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-15 02:27:49 +0530

    style: enhance UI and add responsiveness

--- src/components/VideoTitle.tsx
+++ src/components/VideoTitle.tsx
@@ -6,14 +6,14 @@ type Props = {
 const VideoTitle = ({ title, overview }:Props) => {
 
     return (
-        <div className="w-screen aspect-video bg-linear-to-r from-black pt-[20%] px-24 absolute text-white">
-            <h1 className="text-4xl font-bold">{title}</h1>
-            <p className="py-6 text-m w-1/4">{overview}</p>
-            <div className="">
-            <button className="bg-white text-black p-4 px-12 text-lg hover:bg-white/80 rounded-lg">
+        <div className="px-6 md:px-24 w-screen aspect-video bg-linear-to-r from-black pt-[20%] absolute text-white">
+            <h1 className="text-2xl md:text-4xl font-bold">{title}</h1>
+            <p className="hidden md:inline-block py-6 text-m w-1/4">{overview}</p>
+            <div className="my-4 md:m-0">
+            <button className="bg-white text-black py-1 md:py-4 px-2 md:px-12 text-lg hover:bg-white/80 rounded-lg">
                 ▶️ Play
             </button>
-            <button className="bg-gray-400 mx-2 text-white p-4 px-10 text-lg hover:bg-gray-400/80 rounded-lg">
+            <button className="bg-gray-400 mx-2 hidden md:inline-block text-white p-4 px-10 text-lg hover:bg-gray-400/80 rounded-lg">
                 More Info
             </button>
             </div>
