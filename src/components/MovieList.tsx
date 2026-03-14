1d449f9f7a3e745485906c8eca15e19d0c278d6e
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-15 02:30:22 +0530

    style: enhance UI and add responsiveness

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -13,11 +13,14 @@ type Props = {
 const MovieList = ({ title, movies }:Props) => {
     return (
         <div className="px-6">
-             <h1 className="text-3xl py-4 text-white">{title}</h1>
+             <h1 className="text-lg md:text-3xl py-4 text-white z-10">{title}</h1>
             <div className="flex overflow-x-scroll hide-scrollbar">
+            {/* <div className="flex"> */}
                     {movies.map(movie=> <MovieCard key={movie.id} posterPath={movie?.poster_path}/>)}
             </div>
-        </div>
+            </div>
+        // </div>
+
     );
 };
 
